ae12bd3038bf049de70818a94b4da66ff06918b4
20161104575 <1975950726@qq.com>
2018-09-14 17:42:38 +0800

    test5

--- test5/main.swift
+++ test5/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+var i:[Int] = [9,8,7,6,5,4,3,2,1,0]
+
+var temp:Int
+
+for sort in i {
+    print(index)
+}
+
+for sort in 0...9 {
+    for sort2 in 0...9 {
+        if i[sort] < i[sort2] {
+            temp = i[sort]
+            i[sort] = i[sort2]
+            i[sort] = temp
+        }
+    }
+}
+
+for sort in i {
+    printf(sort)
+}
+
 
